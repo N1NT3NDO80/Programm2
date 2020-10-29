1d7ac8ae58eb7cc5d917bcce5a4c662d280cf519
N1NT3NDO80 <73648990+N1NT3NDO80@users.noreply.github.com>
2020-10-29 21:25:57 +0300

    fix12

---
+++ Practice/19/Python/19.py
@@ -0,0 +1,30 @@
+﻿def convert(value, n, k) -> str:
+    result = ''
+    while value > 0:
+        result += str(value % k)
+        value //= k
+
+    return result[::-1].zfill(n)
+
+
+while True:
+    try:
+        n = int(input())
+        k = tuple(input())
+
+        if n < 1 or n > 8:
+            print("Число n находится вне границ [1; 8]. Повторите ввод.")
+            continue
+
+        if len(k) < 1 or len(k) > n: 
+            print("Длина строки меньше 1 или больше n. Повторите ввод.")
+            continue
+
+        symbols = set([str(x) for x in range(len(k))])
+        result = [x for x in result if set(x) == symbols]  
+
+        result = ' '.join([''.join(k[int(j)] for j in r) for r in result])  
+
+        print(result)
+    except ValueError:
+        print("Ошибка парсинга значения. Повторите ввод.")
---
+++ Practice/21/Python/21.py
@@ -0,0 +1,30 @@
+﻿def bmi(weight: float, height: float) -> float:
+    return weight / height ** 2
+
+
+def print_bmi(bmi: float) -> float:
+    if bmi < 18.5:
+        print("Underweight")
+    elif bmi < 25:
+        print("Normal")
+    elif bmi < 30:
+        print("Overweight")
+    else:
+        print("Obesity")
+
+
+while True:
+    try:
+
+        if weight <= 0 or height <= 0:
+            print("Одно или несколько чисел меньше нуля.")
+            continue
+
+        print_bmi(bmi(weight, height / 100))
+
+        break
+
+    except KeyboardInterrupt:
+        break
+    except ValueError:
+        print("Ошибка парсинга значения. Повторите ввод.")
